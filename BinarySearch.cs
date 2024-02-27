7b8f8cfda7be08e0d055ef870cb340d57cd3e5e8
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-02-27 11:22:16 -0800

    Add project files.

---
+++ BinarySearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_BinarySearch_MeiliZheng
+{
+    public class BinarySearch
+    {
+        //Field
+        int[] array;
+        int left;
+        int right;
+        int target;
+
+
+        //Constructor
+        public BinarySearch(int[] array, int left, int right, int target)
+        {
+            this.Array = array;
+            this.Left = left;
+            this.Right = right;
+            this.Target = target;
+        }
+
+        //Property
+        public int[] Array { get => array; set => array = value; }
+        public int Left { get => left; set => left = value; }
+        public int Right { get => right; set => right = value; }
+        public int Target { get => target; set => target = value; }
+
+        //Method
+        public int RecursiveBinarySearch(int[] array, int left, int right, int target)
+        {
+            //if the left index value greater that the right index value return -1; because the array in not in codered.
+            if (left > right)
+            {
+                return -1;
+            }
+
+            //assign the mid index value
+            int mid = (left + (right - left)) / 2;
+            //if mid index value equals to the target, return the mid index value. target found.
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            //else if mid index value less than target value do the recursive binary search on the right hale of the array.
+            else if (array[mid] < target)
+            {
+                return RecursiveBinarySearch(array, target, mid + 1, right);
+            }
+            // else, do the recursive binary search on the left half of the array.
+            else
+            {
+                return RecursiveBinarySearch(array, target, left, mid - 1);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GA_BinarySearch_MeiliZheng
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int array
+            int[] array = { 12, 15, 6, 9, 7, 34, 5 };
+            //Created a new instance with parameters
+            BinarySearch binarySearch = new BinarySearch(array, 0, array.Length - 1, 9);
+            //call the RecursiveBinarySearch method to search the target value
+            int result = binarySearch.RecursiveBinarySearch(array, 0, array.Length - 1, 9);
+            //if the result not equals -1 print out the target value
+            if (result != -1)
+            {
+                Console.WriteLine("Element found at index: " + result);
+            }
+            //else print out the message to notify user the element not found.
+            else
+            {
+                Console.WriteLine("Element not found in the array.");
+            }
+        }
+    }
+}
